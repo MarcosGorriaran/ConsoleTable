d338bc3996983a3ffa603e5a29c31633293546c6
Marcos <gorriansan@hotmail.com>
2024-03-03 16:05:29 +0100

    Made class Cells and Table to store text information and show it in a table format trough the console, the table class is the one responsible of preparing the string for the console while the Cell class only worries if the text should be bold or not

--- Cell.cs
+++ Cell.cs
@@ -1,14 +1,39 @@
 ﻿
 namespace ConsoleTable
 {
+    /// <summary>
+    /// The Cell class is a class made as an adition to the class Table
+    /// to form the diferent cells a table has.
+    /// </summary>
     public class Cell
     {
+        const string ConsoleBold = "\x1b[1m";
+        const string ConsoleReset = "\x1b[0m";
         private string content;
         private bool header;
-
-        public Cell(string content, bool header) 
+        /// <summary>
+        /// Constructor with biggest logical load
+        /// </summary>
+        /// <param name="content">The text that will be shown on the text of the cell</param>
+        /// <param name="header">If the Cell is a header</param>
+        public Cell(string content, bool header)
         {
-        
+            this.Content = content;
+            this.Header = header;
+        }
+        public string WriteContent()
+        {
+            return this.header ? ConsoleBold+this.Content+ConsoleReset : this.Content;
+        }
+        public string Content
+        {
+            get { return this.content; }
+            set { this.content = value; }
+        }
+        public bool Header
+        {
+            get { return this.header; }
+            set { this.header = value; }
         }
     }
 }
--- Table.cs
+++ Table.cs
@@ -2,6 +2,118 @@
 {
     public class Table
     {
+        const char TableCorner = '+';
+        const char TableHorizontalLine = '-';
+        const char TableVerticalLine = '|';
+        const char WhiteSpace = ' ';
+        const int Halfer = 2;
+        const int AmountCorners = 2;
+        const int CornersForCell = 1;
+        private Cell[,] tableCells;
+        private string title;
+        private int[] longestStringCol;
         
+        public Table(Cell[,] tableCells,string title) 
+        {
+            this.TableCells = tableCells;
+            this.Title = title;
+            
+            
+        }
+        private void InitializeLongestStringCol(int col)
+        {
+            for(int i = 0; i < this.tableCells.GetLength(0); i++)
+            {
+                if (this.longestStringCol[col]< this.tableCells[i, col].Content.Length)
+                {
+                    this.longestStringCol[col] = this.tableCells[i, col].Content.Length;
+                }
+            }
+        }
+        private void InitializeLongestStringCol()
+        {
+            this.longestStringCol = new int[TableCells.GetLength(1)];
+            this.longestStringCol.Initialize();
+            for (int i = 0; i < this.tableCells.GetLength(1); i++)
+            {
+                InitializeLongestStringCol(i);
+            }
+        }
+        private string BuildCenteredCell(int row, int col)
+        {
+            int startBuildingOn = (this.longestStringCol[col] - CornersForCell) / Halfer - this.TableCells[row,col].Content.Length / Halfer;
+            return Table.TableVerticalLine + new string(' ', startBuildingOn) + this.TableCells[row, col].WriteContent() + new string(' ', startBuildingOn);
+        }
+        private string BuildCenteredCell()
+        {
+            int startBuildingOn = (this.TableRowLength()-AmountCorners)/Halfer - this.Title.Length / Halfer;
+            return Table.TableVerticalLine+new string(' ', startBuildingOn)+this.Title+new string(' ',startBuildingOn)+ Table.TableVerticalLine;
+        }
+        private string BuildCell(int row, int col)
+        {
+            int whiteSpaces = this.longestStringCol[col] - this.TableCells[row, col].Content.Length;
+            return Table.TableVerticalLine + this.TableCells[row, col].WriteContent() + new string(' ', whiteSpaces) + Table.TableVerticalLine;
+        }
+        private string BuildRowLine(bool cornerLine)
+        {
+            char cornerContent = cornerLine ? Table.TableCorner : Table.TableVerticalLine;
+            return cornerContent + (new string (Table.TableHorizontalLine,this.TableRowLength()-AmountCorners))+ cornerContent + Environment.NewLine;
+        }
+        private string BuildRowLine()
+        {
+            return BuildRowLine(false);
+        }
+        private string BuildRow(int row)
+        {
+            string rowResult = "";
+            for (int i = 0; i < this.tableCells.GetLength(1); i++)
+            {
+                if (this.TableCells[row, i].Header)
+                {
+                    rowResult += this.BuildCenteredCell(row, i);
+                }
+                else
+                {
+                    rowResult += this.BuildCell(row, i);
+                }
+            }
+            return rowResult+Table.TableVerticalLine+Environment.NewLine;
+        }
+        
+        public int TableRowLength()
+        {
+            int sum = this.longestStringCol.Length+1;
+            for(int i = 0; i < this.longestStringCol.Length; i++)
+            {
+                sum+= this.longestStringCol[i];
+            }
+            return sum;
+        }
+        public Cell[,] TableCells
+        {
+            get { return this.tableCells; }
+            set 
+            {
+                this.tableCells = value;
+                this.InitializeLongestStringCol();
+            }
+        }
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = value; }
+        }
+        public string toTableString()
+        {
+            string returnedValue = this.BuildRowLine(true);
+            returnedValue += this.BuildCenteredCell();
+
+            for(int i = 0; i<this.tableCells.GetLength(0); i++)
+            {
+                returnedValue += this.BuildRowLine();
+                returnedValue += this.BuildRow(i);
+            }
+            return returnedValue+this.BuildRowLine(true);
+        }
     }
 }
